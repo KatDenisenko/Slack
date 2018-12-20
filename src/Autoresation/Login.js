6f766bd2349aba6026c9dfa37541fd12d09ba0be
KatDenisenko <kat.denisenko@gmail.com>
2018-12-20 18:10:06 +0200

    Login form

--- src/Autoresation/Login.js
+++ src/Autoresation/Login.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import { Grid, Header, Icon, Form, Message, Segment, Button } from 'semantic-ui-react';
+import {NavLink} from 'react-router-dom';
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello, I am Login form</h1>
-            </div>
+            <Grid textAlign='center' verticalAlign='middle' className='app'>
+            <Grid.Column style={{maxWidth: 450}}>
+            <Header as='h2' icon color = 'blue' textAlign='center'>
+            <Icon name='smile' color = 'blue'/>
+            Login for Slack Clone
+            </Header>
+            <Form size='large'>
+            <Segment>
+
+            
+<Form.Input
+            fluid
+            name='email'
+            icon='mail'
+            iconPosition='left'
+            placeholder='Mail'
+            type='email'
+            // onChange={this.handlerChange}
+            // value={this.state.email}
+            // className={this.handleInput(this.state.errors, 'email')}
+            />
+
+<Form.Input
+            fluid
+            name='password'
+            icon='lock'
+            iconPosition='left'
+            placeholder='Password'
+            type='password'
+            // onChange={this.handlerChange}
+            // value={this.state.password}
+            // className={this.handleInput(this.state.errors, 'password')}
+            />
+
+
+
+            <Button color='blue' fluid size='large'>Submit</Button>
+
+            </Segment>
+            </Form>
+            {/* {this.state.errors.length>0 && (
+                <Message error>
+                <h3>Error</h3>
+                {this.state.errors.map(el => <p key={el.message}>{el.message}</p>)}
+                </Message>
+            )
+
+            } */}
+            <Message>
+                Don't have an acount?
+                <NavLink to='/registr'>&nbsp;&nbsp;Registration</NavLink>
+            </Message>
+
+            </Grid.Column>
+            </Grid>
         );
     }
 }
