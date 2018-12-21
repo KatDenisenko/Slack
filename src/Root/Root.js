da7427f08e9c2e5a9c3b2cd2ebacac9d8678200b
KatDenisenko <kat.denisenko@gmail.com>
2018-12-21 11:10:37 +0200

    re direct

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route, withRouter} from 'react-router-dom';
 import App from '../App';
 import Login from '../Autoresation/Login';
-import Registration from '../Autoresation/Registration'
+import Registration from '../Autoresation/Registration';
+import firebase from 'firebase';
 
 class Root extends Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user=>{
+          if(user) {
+            console.log(user)
+            this.props.history.push('/');
+          }
+        })
+      }
     render() {
         return (
             <div>
@@ -19,4 +28,4 @@ class Root extends Component {
     }
 }
 
-export default Root;
+export default  withRouter(Root);//withRouter HOC который принимает аргументом компонент и даёт этому объекту доступ к истории браузера history
