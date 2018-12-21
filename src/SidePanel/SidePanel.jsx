b9754b9c4c516f98387d3cc3ca3e970bfd8dcdcc
KatDenisenko <kat.denisenko@gmail.com>
2018-12-21 13:46:30 +0200

    app markup

--- src/SidePanel/SidePanel.jsx
+++ src/SidePanel/SidePanel.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import UserPanel from '../UserPanel/UserPanel';
 
 class SidePanel extends Component {
     render() {
         return (
-            <div>
-                 <p>Hello I am SidePanel</p>
-            </div>
+            <Menu
+            size='large'
+            inverted
+            fixed='left'
+            vertical
+            style={{background:'#4c3c4c', fontSize: '1.2rem',}}>
+            <UserPanel/>
+            </Menu>
+           
         );
     }
 }
---
+++ src/UserPanel/UserPanel.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+
+class UserPanel extends Component {
+
+    dropdownOptions=()=>[
+        {
+            key:'user',
+            text:<span><Icon name='smile outline'/>Signed in as <strong>User</strong></span>,
+            disabled:true,
+        },
+        {
+            key:'avatar',
+            text:<span><Icon name='id card'/>Change Avatar</span>,
+
+            
+        },
+        {
+            key:'out',
+            text:<span><Icon name='sign-out'/> Sign out</span>,
+            
+        }
+    ]
+
+    render() {
+        return (
+            <Grid style={{
+                background: '4c3c4c'
+            }}>
+                <Grid.Column>
+                    <Grid.Row style={{
+                        padding: '1.2rem',
+                        margin: '0',
+                    }}>
+                    <Header inverted floated='left' as='h2'>
+                        <Icon name='cloud'/>
+                        <Header.Content>Slack clone</Header.Content>
+                    </Header>
+
+                    </Grid.Row>
+                    <Header style={{padding: '0.25rem'}} as='h4' inverted>
+                        <Dropdown trigger={
+                            <span>User</span>
+                        } options={this.dropdownOptions()}/>
+                    </Header>
+                </Grid.Column>
+            </Grid>
+                
+            
+        );
+    }
+}
+
+export default UserPanel;
