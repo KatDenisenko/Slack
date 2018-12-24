9ae5735fd6de55588dcb556d9f2618bb91f8732c
KatDenisenko <kat.denisenko@gmail.com>
2018-12-24 14:48:35 +0200

    markup ColorPanel

--- src/ColorPanel/ColorPanel.jsx
+++ src/ColorPanel/ColorPanel.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react';
 
 class ColorPanel extends Component {
     render() {
         return (
-            <div>
-                <p>Hello I am ColorPanel</p>
-            </div>
+            <Sidebar
+            as={Menu}
+            icon='labeled'
+            inverted
+            visible
+            vertical
+            width='very thin'>
+            <Divider/>
+            <Button icon='add' size='small' color='blue'/>
+            </Sidebar>
+            
         );
     }
 }
