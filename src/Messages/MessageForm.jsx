acc6bc85631e2cecf0ef81991d7f949adec27dee
KatDenisenko <kat.denisenko@gmail.com>
2018-12-24 16:50:48 +0200

    add messages to DB

--- src/Messages/MessageForm.jsx
+++ src/Messages/MessageForm.jsx
@@ -1,12 +1,70 @@
 import React, { Component } from 'react';
 import { Segment, Input, Button } from 'semantic-ui-react';
+import {connect} from 'react-redux';
+import firebase from '../fireBase'
 
 class MessageForm extends Component {
+
+    state={
+        message:'',
+        loading: false,
+        errors: [],
+    }
+    addMessage =()=> {
+        const {message}=this.state;
+        const {messagesRef,currentChanel}=this.props;
+            if (message) {
+                this.setState({
+                    loading:true,
+                })
+                messagesRef
+                .child(currentChanel.id)
+                .push()
+                .set(this.createMessage())
+                .then(()=>{
+                    this.setState({
+                        loading:false,
+                        message: ''
+                    })
+                })
+                .catch(err=>{
+                    this.setState({
+                        loading: false,
+                errors:this.state.errors.concat(err)
+                    })
+                })
+            } 
+        }
+ 
+
+    handleChange=(e)=>{
+        let value =e.target.value;
+        let key=e.target.name;
+        this.setState({
+                [key]:value,
+        })
+    }
+
+    createMessage =()=> {
+        const message={
+            content: this.state.message,
+            time: firebase.database.ServerValue.TIMESTAMP,
+            user: {
+                id:this.props.currentUser.uid,
+                name:this.props.currentUser.displayName,
+                avatar:this.props.currentUser.photoURL
+            }
+        }
+        console.log(message);
+        return message;
+    }
+
     render() {
         return (
             <div>
                 <Segment className='message_form'>
                 <Input
+                    onChange={this.handleChange}
                     fluid
                     name='message'
                     style={{
@@ -14,9 +72,10 @@ class MessageForm extends Component {
                     }}
                     label={<Button icon='add'/>}
                     labelPosition='left'
-                    placeholder='Wright your message'/>
+                    placeholder='Wright your message'
+                    value={this.state.message}/>
                 <Button.Group icon widths='2'>
-                    <Button color='orange' content='Add Reply' labelPosition='left' icon='edit'/>
+                    <Button onClick = {this.addMessage} color='orange' content='Add Reply' labelPosition='left' icon='edit'/>
                     <Button color='teal' content='Upload media' labelPosition='right' icon='cloud upload'/>
                 </Button.Group>
                 </Segment>
@@ -25,4 +84,12 @@ class MessageForm extends Component {
     }
 }
 
-export default MessageForm;
+function MSTP (state) {
+    return {
+        currentUser:state.user.currentUser,
+        currentChanel:state.chanel,
+    }
+
+}
+
+export default connect (MSTP,null)(MessageForm);
--- src/Messages/Messages.jsx
+++ src/Messages/Messages.jsx
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import MessageForm from './MessageForm'
 import MessageHeader from './MessageHeader'
 import { Segment, Comment } from 'semantic-ui-react';
+import firebase from '../fireBase'
 class Messages extends Component {
+
+    state={
+        messagesRef: firebase.database().ref('messages')
+    }
     render() {
+        const {messagesRef}=this.state
+       
+
         return (
             <React.Fragment>
                  <MessageHeader/>
             <Segment>
-                <Comment.Group className='message'>
-                
+                <Comment.Group className='messages'>
+
                 </Comment.Group>
             </Segment>
-            <MessageForm/>
+            <MessageForm messagesRef={messagesRef}/>
             </React.Fragment>
         );
     }
